b4354ce37d9f840fddbbd35fc9e831549c8e1fa7
Shreyash pal <148391511+ShreyashPal461@users.noreply.github.com>
2026-08-02 13:09:12 +0530

    Time: 1 ms (11.76%), Space: 43.2 MB (53.03%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -1,14 +1,40 @@
+import java.util.*;
+
 class Solution {
-    static void postorder(TreeNode root, List<Integer> ans){
-        if(root==null) return;
-        postorder(root.left,ans);
-        postorder(root.right,ans);
-        ans.add(root.val);
-    }
     public List<Integer> postorderTraversal(TreeNode root) {
+
         List<Integer> ans = new ArrayList<>();
-        postorder(root,ans);
+        Stack<TreeNode> st = new Stack<>();
+        if (root != null) st.push(root);
+
+        while (!st.isEmpty()) {
+            TreeNode top = st.pop();
+            ans.add(top.val);
+
+            if (top.left != null)
+                st.push(top.left);
+
+            if (top.right != null)
+                st.push(top.right);
+        }
+
+        Collections.reverse(ans);
+
         return ans;
-        
     }
 }
+
+// class Solution {
+//     static void postorder(TreeNode root, List<Integer> ans){
+//         if(root==null) return;
+//         postorder(root.left,ans);
+//         postorder(root.right,ans);
+//         ans.add(root.val);
+//     }
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         postorder(root,ans);
+//         return ans;
+        
+//     }
+// }
