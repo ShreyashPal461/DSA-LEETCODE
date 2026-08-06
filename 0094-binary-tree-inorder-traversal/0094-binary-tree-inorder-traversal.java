d7d0028ab4221e0b0188985e26e8433890d05e45
Shreyash pal <148391511+ShreyashPal461@users.noreply.github.com>
2026-08-06 12:34:53 +0530

    Time: 0 ms (100%), Space: 43.1 MB (52.64%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,15 +1,37 @@
-class Solution {
-    static void inorder(TreeNode root, List<Integer> ans){
-        if(root==null) return;
-        inorder(root.left,ans);
-        ans.add(root.val);
-        inorder(root.right,ans);
-
-    }
-    public List<Integer> inorderTraversal(TreeNode root) {
+class Solution{
+    public List<Integer> inorderTraversal(TreeNode root){
         List<Integer> ans = new ArrayList<>();
-        inorder(root,ans);
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode temp = root;
+        while(true){
+            if(temp!=null){
+                st.push(temp);
+                temp=temp.left;
+
+            }else{
+                if(st.size()==0) break;
+                TreeNode top = st.pop();
+                ans.add(top.val);
+                temp=top.right;
+            }
+
+        }
         return ans;
-        
     }
 }
+
+// class Solution {
+//     static void inorder(TreeNode root, List<Integer> ans){
+//         if(root==null) return;
+//         inorder(root.left,ans);
+//         ans.add(root.val);
+//         inorder(root.right,ans);
+
+//     }
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         inorder(root,ans);
+//         return ans;
+        
+//     }
+// }
