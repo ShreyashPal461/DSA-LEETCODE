8addcc79d299053d3fac06770df34308fad3d7f8
Shreyash pal <148391511+ShreyashPal461@users.noreply.github.com>
2026-08-17 00:06:00 +0530

    Time: 0 ms (100%), Space: 43.1 MB (38.95%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
@@ -1,19 +1,37 @@
-
+// its a reverse inorder solution
 class Solution {
-    public void inorder(TreeNode root,List<TreeNode> ans){
+    static int sum;
+    public void reverseinorder(TreeNode root){
         if(root==null) return;
-        inorder(root.left,ans);
-        ans.add(root);
-        inorder(root.right,ans);
+        reverseinorder(root.right);
+        root.val+=sum;
+        sum=root.val;
+        reverseinorder(root.left);
     }
     public TreeNode bstToGst(TreeNode root) {
-        List<TreeNode> ans = new ArrayList<>();
-        inorder(root,ans);
-        int n = ans.size();
-        for(int i=n-2;i>=0;i--){
-            ans.get(i).val+=ans.get(i+1).val;
-        }
+        sum=0;
+        reverseinorder(root);
         return root;
         
     }
 }
+
+// both are solution 
+// class Solution {
+//     public void inorder(TreeNode root,List<TreeNode> ans){
+//         if(root==null) return;
+//         inorder(root.left,ans);
+//         ans.add(root);
+//         inorder(root.right,ans);
+//     }
+//     public TreeNode bstToGst(TreeNode root) {
+//         List<TreeNode> ans = new ArrayList<>();
+//         inorder(root,ans);
+//         int n = ans.size();
+//         for(int i=n-2;i>=0;i--){
+//             ans.get(i).val+=ans.get(i+1).val;
+//         }
+//         return root;
+        
+//     }
+// }
